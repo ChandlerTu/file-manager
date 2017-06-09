95e50748bb907ae70d1179fa65a5a1e93f532f23
ChandlerTu <chandlertu@chandlertu.com>
2017-06-09 17:10:18 +0800

    DirectoryCounter

---
+++ src/test/java/com/chandlertu/file/manager/DirectoryCounterTest.java
@@ -0,0 +1,16 @@
+package com.chandlertu.file.manager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class DirectoryCounterTest {
+
+	@Test
+	public void test() {
+		Path directory = Paths.get("C:\\нд╪Ч");
+		DirectoryCounter.count(directory);
+	}
+
+}
