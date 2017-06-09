0dc6ae3d80f1b76f45136edf9b2e222d68a338dd
ChandlerTu <chandlertu@chandlertu.com>
2017-06-09 17:52:43 +0800

    FileCounter

---
+++ src/main/java/com/chandlertu/file/manager/FileCounter.java
@@ -0,0 +1,24 @@
+package com.chandlertu.file.manager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileCounter {
+
+	public static void count(Path parent) {
+		try (Stream<Path> paths = Files.walk(parent)) {
+			Map<Boolean, Long> map = paths
+					.collect(Collectors.partitioningBy(Files::isDirectory, Collectors.counting()));
+			long fileSize = map.get(false);
+			long directorySize = map.get(true) - 1;
+			System.out.println(fileSize + " 个文件，" + directorySize + " 个文件夹");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/test/java/com/chandlertu/file/manager/FileCounterTest.java
+++ src/test/java/com/chandlertu/file/manager/FileCounterTest.java
@@ -5,12 +5,12 @@ import java.nio.file.Paths;
 
 import org.junit.Test;
 
-public class DirectoryCounterTest {
+public class FileCounterTest {
 
 	@Test
 	public void test() {
 		Path directory = Paths.get("C:\\нд╪Ч");
-		DirectoryCounter.count(directory);
+		FileCounter.count(directory);
 	}
 
 }
