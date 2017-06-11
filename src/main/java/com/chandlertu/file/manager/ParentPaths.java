a49dc18b0100e884c747ced7279bf34b4f8771ee
Chandler Tu <chandlertu@chandlertu.com>
2017-06-11 16:04:52 +0800

    Changed ParentPaths.java

--- src/main/java/com/chandlertu/file/manager/ParentPaths.java
+++ src/main/java/com/chandlertu/file/manager/ParentPaths.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class ParentPaths {
 
-	public static final Path FILES = Paths.get("C:\\文件");
+	public static final Path FILES = Paths.get("D:\\Files");
 	public static final Path FAVORITES = FILES.resolve("收藏");
 	public static final Path VIDEOS = FILES.resolve("视频");
 	public static final Path PICTURES = FILES.resolve("图片");
